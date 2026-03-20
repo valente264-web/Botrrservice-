af964af07c19f2231c843e08ea74c9f3570b4420
Rui Valente <valente264@hotmail.com>
2026-03-20 17:31:36 -0300

    fix render port

--- index.js
+++ index.js
@@ -695,3 +695,17 @@ await fluxos[fluxoAtual](sock, jid, id, texto)
 }
 
 iniciarBot()
+// ================= SERVIDOR WEB (RENDER) =================
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('✅ RR SERVICE BOT ONLINE')
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log('🌐 Servidor web rodando na porta ' + PORT)
+})
